262c3ece11245d33a5c6b0d43bfdd35de84039f7
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-11-06 19:34:02 +0000

    add: first commit

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿// using System;
+
+// class Program
+// {
+//   static void Main(string[] args)
+//   {
+//     // Your code here
+//     Console.WriteLine("Hello, World");
+//   }
+// }
+
+// understanding Console.WriteLine()
+
+// using System;
+
+// namespace HelloWorld
+// {
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("Hello Marcio!");
+//     }
+//   }
+// }
+
+// understanding Console.ReadLine()
+
+// using System;
+
+// namespace GettingInput
+// {
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("How old are you?");
+//       string input = Console.ReadLine();
+//       Console.WriteLine($"You are {input} years old!");
+//     }
+//   }
+// }
+
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // console creature
+      Console.WriteLine(" .-.");
+      Console.WriteLine("o   o");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("creature scum");
+    }
+  }
+}
+
+// datatypes
+
+ using System;
+
+namespace Form
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Create Variables
+      string name = "Shadow";
+      string breed = "Golden Retriever";
+      int age = 5;
+      double weight = 65.22;
+      bool spayed = true;
+      // Print variables to the console
+      Console.WriteLine(name);
+      Console.WriteLine(breed);
+      Console.WriteLine(age);
+      Console.WriteLine(weight);
+      Console.WriteLine(spayed);
+    }
+  }
+}
+
+// datatype conversions 
+
+using System;
+
+namespace FavoriteNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Ask user for fave number
+      Console.Write("Enter your favorite number!: ");
+      int faveNumber = Convert.ToInt32(Console.ReadLine());
+      // Turn that answer into an int
+
+
+
+    }
+  }
+}
+
+
