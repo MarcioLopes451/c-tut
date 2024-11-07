8244471b470ec0a590bdb3dc35dcb92184e75201
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-11-07 19:37:55 +0000

    add: intro into csharp

--- Program.cs
+++ Program.cs
@@ -41,70 +41,93 @@
 //   }
 // }
 
-using System;
+// using System;
 
-namespace ConsoleCreatures
-{
-  class Program
-  {
-    static void Main()
-    {
-      // console creature
-      Console.WriteLine(" .-.");
-      Console.WriteLine("o   o");
-      Console.WriteLine("| O |");
-      Console.WriteLine("|   |");
-      Console.WriteLine("'~~~'");
-      Console.WriteLine("creature scum");
-    }
-  }
-}
+// namespace ConsoleCreatures
+// {
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       // console creature
+//       Console.WriteLine(" .-.");
+//       Console.WriteLine("o   o");
+//       Console.WriteLine("| O |");
+//       Console.WriteLine("|   |");
+//       Console.WriteLine("'~~~'");
+//       Console.WriteLine("creature scum");
+//     }
+//   }
+// }
 
 // datatypes
 
- using System;
+//  using System;
 
-namespace Form
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      // Create Variables
-      string name = "Shadow";
-      string breed = "Golden Retriever";
-      int age = 5;
-      double weight = 65.22;
-      bool spayed = true;
-      // Print variables to the console
-      Console.WriteLine(name);
-      Console.WriteLine(breed);
-      Console.WriteLine(age);
-      Console.WriteLine(weight);
-      Console.WriteLine(spayed);
-    }
-  }
-}
+// namespace Form
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Create Variables
+//       string name = "Shadow";
+//       string breed = "Golden Retriever";
+//       int age = 5;
+//       double weight = 65.22;
+//       bool spayed = true;
+//       // Print variables to the console
+//       Console.WriteLine(name);
+//       Console.WriteLine(breed);
+//       Console.WriteLine(age);
+//       Console.WriteLine(weight);
+//       Console.WriteLine(spayed);
+//     }
+//   }
+// }
 
 // datatype conversions 
 
+// using System;
+
+// namespace FavoriteNumber
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Ask user for fave number
+//       Console.Write("Enter your favorite number!: ");
+//       int faveNumber = Convert.ToInt32(Console.ReadLine());
+//       // Turn that answer into an int
+
+
+
+//     }
+//   }
+// }
+
+
 using System;
 
-namespace FavoriteNumber
+public class GalacticTravelAgency
 {
-  class Program
+  public static void Main(string[] args)
   {
-    static void Main(string[] args)
-    {
-      // Ask user for fave number
-      Console.Write("Enter your favorite number!: ");
-      int faveNumber = Convert.ToInt32(Console.ReadLine());
-      // Turn that answer into an int
-
-
-
-    }
+    // Your code goes here
+    string passengerName = "Zara";
+    int passengerAge = 28;
+    string ticketType = "First Class";
+    string preferredPlanet = "Mars";
+    Console.WriteLine(passengerName);
+    Console.WriteLine(passengerAge + 1);
+    Console.WriteLine(ticketType);
+    Console.WriteLine(preferredPlanet);
+    double passengerAgeDouble = (double)passengerAge;
+    Console.WriteLine(passengerAgeDouble);
+    double passengerAgeDouble2 = passengerAge;
+    Console.WriteLine(passengerAgeDouble2);
+    string passengerAgeString = Convert.ToString(passengerAge);
+    Console.WriteLine(passengerAgeDouble2);
   }
 }
-
-
