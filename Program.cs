8dc2f4496cd9c0f629960ae9bb6eba1a4e78a9ab
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-11-11 20:34:58 +0000

    add: operations in c#

--- Program.cs
+++ Program.cs
@@ -131,3 +131,54 @@ public class GalacticTravelAgency
     Console.WriteLine(passengerAgeDouble2);
   }
 }
+
+using System;
+
+namespace Numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Number of pizza shops
+      int pizzaShops = 4332;
+
+
+      // Number of employees
+      int totalEmployees = 86928;
+
+
+      // Revenue
+      decimal revenue = 390819.28m;
+
+
+      // Log the values to the console:
+      Console.WriteLine(pizzaShops);
+      Console.WriteLine(totalEmployees);
+      Console.WriteLine(revenue);
+
+      // Your Age
+      int userAge = 24;
+
+      // Length of years on Jupiter (in Earth years)
+      double jupiterYears = 11.86;
+
+      // Age on Jupiter
+      double jupiterAge = userAge / jupiterYears;
+
+      // Time to Jupiter
+      double journeyToJupiter = 6.142466;
+
+      // New Age on Earth
+      double newEarthAge = userAge + journeyToJupiter;
+      // New Age on Jupiter
+
+      double newJupiterAge = newEarthAge / jupiterYears;
+      // Log calculations to console
+      Console.WriteLine(newEarthAge);
+      Console.WriteLine(jupiterAge);
+      Console.WriteLine(newJupiterAge);
+
+    }
+  }
+}
