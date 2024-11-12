e4a5222eb5ac29184557f8da06b5656764b9029a
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-11-12 22:08:28 +0000

    add: mad libs project

--- Program.cs
+++ Program.cs
@@ -321,3 +321,46 @@ namespace PrideAndPrejudice
       }
     }
   }
+
+  using System;
+
+namespace MadLibs
+  {
+    class Program
+    {
+      static void Main(string[] args)
+      {
+        /*
+        This program ...
+        Author: ...
+        */
+        // Let the user know that the program is starting:
+        Console.WriteLine("the mad libs has started");
+
+        // Give the Mad Lib a title:
+        string title = "whatever title";
+
+        Console.WriteLine(title);
+        // Define user input and variables:
+        Console.Write("Enter a name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("give me three adjectives...");
+        string adjectiveOne = Console.ReadLine();
+        string adjectiveTwo = Console.ReadLine();
+        string adjectiveThree = Console.ReadLine();
+        Console.WriteLine("give me one verb...");
+        string verbOne = Console.ReadLine();
+        Console.WriteLine("give me two nouns...");
+        string nounOne = Console.ReadLine();
+        string nounTwo = Console.ReadLine();
+        Console.WriteLine("now i need you to give me a animal...");
+        string animal = Console.ReadLine();
+        // The template for the story:
+
+        string story = $"This morning {name} woke up feeling {adjectiveOne}. 'It is going to be a {adjectiveTwo} day!' Outside, a bunch of {animal}s were protesting to keep food in stores. They began to {verbOne} to the rhythm of the {nounOne}, which made all the fruits very {adjectiveThree}. Concerned, {name} texted superhero, who flew {name} to usa and dropped {name} in a puddle of frozen dessert. {name} woke up in the year 2021, in a world where {nounTwo}s ruled the world.";
+
+
+        // Print the story:
+        Console.WriteLine(story);
+      }
+    }
