9ebf46eca79a0313f3436c6010c5b3f200fbeb16
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-11-15 23:11:13 +0000

    Add: project two

--- Program2.cs
+++ Program2.cs
@@ -52,3 +52,141 @@ public class SpaceExpedition
     Console.WriteLine(canAcceptMoreCrew);
   }
 }
+
+
+// conditional statements 
+
+using System;
+
+namespace IfStatement
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int socks = 6;
+      if (socks <= 3)
+      {
+        Console.WriteLine("Time to do laundry!");
+      };
+
+      int people = 11;
+      string weather = "nice";
+      if (people <= 10 && weather == "nice")
+      {
+        Console.WriteLine("SaladMart");
+      }
+      else
+      {
+        Console.WriteLine("Soup N Sandwich");
+      };
+
+      double ph = 7;
+      if (ph < 7)
+      {
+        Console.WriteLine("Acidic");
+      }
+      else if (ph > 7)
+      {
+        Console.WriteLine("Basic");
+      }
+      else
+      {
+        Console.WriteLine("Neutral");
+      }
+
+      double ph = 14;
+
+      switch (ph)
+      {
+        case <= 3:
+          Console.WriteLine("Very Acidic");
+          break;
+        case < 7:
+          Console.WriteLine("Acidic");
+          break;
+        case >= 11:
+          Console.WriteLine("Very Basic");
+          break;
+        case > 7:
+          Console.WriteLine("Basic");
+          break;
+        default:
+          Console.WriteLine("Neutral");
+          break;
+      }
+
+      int pepperLength = 4;
+      string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
+      Console.WriteLine(message);
+    }
+  }
+}
+
+
+// project password checker
+
+using System;
+
+namespace PasswordChecker
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      int minLength = 8;
+      string uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+      string lowercase = "qwertyuiopasdfghjklzxcvbnm";
+      string digits = "1234567890";
+      string specialChars = "!@£$%^&*?";
+
+      Console.WriteLine("enter password");
+      string input = Console.ReadLine();
+      Console.WriteLine(input);
+
+      int score = 0;
+      int inputLength = input.Length;
+
+      if (inputLength >= minLength)
+      {
+        score++;
+      }
+      if (Tools.Contains(input, uppercase))
+      {
+        score++;
+      };
+      if (Tools.Contains(input, lowercase))
+      {
+        score++;
+      };
+      if (Tools.Contains(input, digits))
+      {
+        score++;
+      };
+      if (Tools.Contains(input, specialChars))
+      {
+        score++;
+      };
+      Console.WriteLine(score);
+
+      switch (score)
+      {
+        case 4:
+          Console.WriteLine("Password is really strong");
+          break;
+        case 3:
+          Console.WriteLine("Password is strong");
+          break;
+        case 2:
+          Console.WriteLine("Password is medium");
+          break;
+        case 1:
+          Console.WriteLine("Password is weak");
+          break;
+        default:
+          Console.WriteLine("password doesnt meet standards.");
+          break;
+      }
+    }
+  }
+}
